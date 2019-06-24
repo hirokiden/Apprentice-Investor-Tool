0a6465b95a42b2eb839d305ad98ca8ca8e2c0386
hirokiden <hd1043@stern.nyu.edu>
2019-06-24 15:14:36 -0400

    Created most active stock template

---
+++ app/most_active_stocks.py
@@ -0,0 +1,32 @@
+# python most_active_stocks.py
+
+
+#!/usr/bin/env python
+
+try:
+    # For Python 3.0 and later
+    from urllib.request import urlopen
+except ImportError:
+    # Fall back to Python 2's urllib2
+    from urllib2 import urlopen
+
+import json
+
+def get_jsonparsed_data(url):
+    """
+    Receive the content of ``url``, parse it as JSON and return the object.
+
+    Parameters
+    ----------
+    url : str
+
+    Returns
+    -------
+    dict
+    """
+    response = urlopen(url)
+    data = response.read().decode("utf-8")
+    return json.loads(data)
+
+url = ("https://financialmodelingprep.com/api/stock/actives?datatype=json")
+print(get_jsonparsed_data(url))
