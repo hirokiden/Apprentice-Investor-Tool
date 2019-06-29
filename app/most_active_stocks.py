c2b5e66f84795dab6f6c2471d23bde3424cf4a6c
hirokiden <hd1043@stern.nyu.edu>
2019-06-29 14:56:23 -0400

    Updated with unique variables

--- app/most_active_stocks.py
+++ app/most_active_stocks.py
@@ -17,13 +17,13 @@ def usd_format(my_price):
     return "${0:,.2f}".format(my_price) # This UDF will change numerical denomination to currency and cents (2 digits) format when passed through
 
 
-#!/usr/bin/env python
+# Code dervied from the website: https://financialmodelingprep.com/developer/docs/most-actives-stock-market-data-free-api/
 
 try:
-    # For Python 3.0 and later
+    # For use on Python 3.0 +
     from urllib.request import urlopen
 except ImportError:
-    # Fall back to Python 2's urllib2
+    # Revise back to Python 2.0 if necessary using except
     from urllib2 import urlopen
 
 import json
@@ -60,7 +60,7 @@ most_active_parser_response = get_jsonparsed_data(url)
 
 
 print("You are pulling this information at:", date_time)
-print("The Most Active Stock Traded is:")
+print("The Most Active Stocks Traded Are:")
 
 for k in most_active_parser_response.keys():
     ticker = most_active_parser_response[k]
@@ -77,7 +77,7 @@ for k in most_active_parser_response.keys():
     print("Company Name:", mas_comp_name)
     print("Current Price:", usd_format( float(mas_price) ) )
     print("Price Change:", usd_format( float(mas_change) ) )
-    print("Price Change %:", usd_format( float(mas_change_pct) ) )
+    print("Price Change %:", mas_change_pct)
     
 
 # parameters for print 
